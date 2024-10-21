da675cb69bf6fcb317dc27a67f47cbb032d3d424
whs2233 <2246478872@qq.com>
2024-10-21 09:29:48 +0800

    feat(backend): 更新客户端启动器功能
    - 添加检查注册表以获取启动路径的功能
    - 优化启动应用程序的逻辑，使用从注册表获取的路径
    - 移除不必要的代码和重复功能
    - 改进错误处理和日志输出

--- backend/client_launcher.py
+++ backend/client_launcher.py
@@ -1,29 +1,77 @@
+import os
 import subprocess
 import time
 import psutil
+import winreg
+
+
+def is_registry_startup(params):
+    # 定义要检查的注册表路径和键名
+    registry_path = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
+    key_name = 'XbladePanel'
+
+    try:
+        # 打开注册表项
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, registry_path) as key:
+            try:
+                # 尝试读取指定键名的值
+                value = winreg.QueryValueEx(key, key_name)
+                print(f"找到注册表项: {key_name}，值为: {value[0]}")
+                return True
+            except FileNotFoundError:
+                # 如果键不存在
+                print(f"未找到注册表项: {key_name}")
+                return False
+    except PermissionError:
+        print("权限错误: 请以管理员身份运行此脚本")
+        return False
+
+def get_startup_registry():
+    if is_registry_startup([]):
+        # 定义要检查的注册表路径和键名
+        registry_path = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
+        key_name = 'XbladePanel'
+
+        try:
+            # 打开注册表项
+            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, registry_path) as key:
+                try:
+                    # 尝试读取指定键名的值
+                    value = winreg.QueryValueEx(key, key_name)
+                    print(f"找到注册表项: {key_name}，值为: {value[0]}")
+                    return value[0]
+                except BaseException as e:
+                    # 如果键不存在
+                    print(f"未找到注册表项: {e}")
+                    return False
+        except BaseException as e:
+            print(f"Error reading registry value: {e}")
+            return False
+    return False
+
+
 
 
 def launch_application():
+    path = get_startup_registry()
+    project_path = path.replace("client_launcher.exe","")
+    program_path = project_path + "XBLADE.exe"
     try:
-        # 启动XBLADE.exe，并使用runas以管理员权限运行
-        subprocess.Popen(["XBLADE.exe"], shell = True)
-        print("启动 XBLADE.exe ...")
-
+        subprocess.Popen([program_path], shell = True, cwd=os.path.dirname(project_path))
         # 监控是否启动成功
-        while True:
+        for a in range(10):
             # 检查所有进程
             for proc in psutil.process_iter(['pid', 'name']):
                 # 如果找到XBLADE.exe进程，退出此脚本
                 if proc.info['name'] == 'XBLADE.exe':
                     print("XBLADE.exe 启动成功，退出脚本")
+                    time.sleep(5)
                     return
-
             # 暂停1秒钟，避免无限循环占用CPU
-            time.sleep(1)
+            time.sleep(3)
 
     except Exception as e:
         print(f"出现错误: {e}")
+    return
 
-
-if __name__ == '__main__':
-    launch_application()
+launch_application()
